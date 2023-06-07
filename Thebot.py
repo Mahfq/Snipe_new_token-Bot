eaccab97fcf68540c97a157cb3230a55c8c82a2d
Mahfq <135901046+Mahfq@users.noreply.github.com>
2023-06-07 23:40:12 +0200

    Update Thebot.py

--- Thebot.py
+++ Thebot.py
@@ -7,20 +7,26 @@ web3 = Web3(Web3.HTTPProvider(Provider_url))
 
 if web3.is_connected():
     print("Connecté à Ethereum")
-    
+
+#Smart contract ERC20 ABI  
 abi = json.loads('[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_initialSupply","type":"uint256"},{"name":"_name","type":"string"},{"name":"_symbol","type":"string"},{"name":"_decimals","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]')
+
 latest_block_number = web3.eth.block_number
 
 def main():
+    #Find new block
     global latest_block_number 
     block_number = web3.eth.block_number  
     if block_number != latest_block_number: 
         latest_block_number = block_number
+        #Find transactions in this block
         block = web3.eth.get_block(latest_block_number, full_transactions=True)
         transactions = block['transactions']
         print("Nombre de transactions dans le bloc" ,latest_block_number,":", len(transactions))
+        #Process transaction data
         for tx in transactions:
             to_address = tx['to']
+            #It's smart contract
             if to_address is None or to_address == "0x0":
                 info_transac = web3.eth.get_transaction_receipt(tx.hash.hex())
                 address = info_transac['contractAddress']
