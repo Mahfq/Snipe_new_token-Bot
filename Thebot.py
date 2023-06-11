ad5806adff068040caad8a998e063f08c9d9b4a7
Mahfq <135901046+Mahfq@users.noreply.github.com>
2023-06-11 19:56:40 +0200

    Update Thebot.py

--- Thebot.py
+++ Thebot.py
@@ -1,61 +1,76 @@
 from web3 import Web3
 import json
 
-# add your blockchain connection information
 with open('secret.json', 'r') as file:
     data = json.load(file)
 
-provider_url = data['file']
-web3 = Web3(Web3.HTTPProvider(provider_url))
-if web3.is_connected():
-    print("Connecté à Ethereum")
+with open('ABI_ERC20.json', 'r') as abi_file:
+    abi_erc20 = json.load(abi_file)
 
-#Smart contract ERC20 ABI  
-abi = json.loads('[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_initialSupply","type":"uint256"},{"name":"_name","type":"string"},{"name":"_symbol","type":"string"},{"name":"_decimals","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]')
+with open('ABI_ERC721.json', 'r') as abi_file:
+    abi_erc721 = json.load(abi_file)
 
+def connection():
+    web3 = Web3(Web3.HTTPProvider(data['file']))
+    if web3.is_connected():
+        print("Connecté à Ethereum")
+    return web3
+
+web3 = connection()
 latest_block_number = web3.eth.block_number
 
-def main():
-    #Find new block
-    global latest_block_number 
-    block_number = web3.eth.block_number  
-    if block_number != latest_block_number: 
+def nb_block():
+    global latest_block_number
+    block_number = web3.eth.block_number
+    if block_number != latest_block_number:
         latest_block_number = block_number
-        #Find transactions in this block
         block = web3.eth.get_block(latest_block_number, full_transactions=True)
+        print("Nouveau blocs", ":", latest_block_number)
         transactions = block['transactions']
-        print("Nombre de transactions dans le bloc" ,latest_block_number,":", len(transactions))
-        #Process transaction data
+        return transactions
+
+def is_erc20(contract_address):
+    erc20_contract = web3.eth.contract(address=contract_address, abi=abi_erc20)
+    try:        
+        name = erc20_contract.functions.name().call()
+        symbol = erc20_contract.functions.symbol().call()
+        totalsupply = erc20_contract.functions.totalSupply().call()
+        print()
+        print(
+            f"{contract_address} - Ce contract est ERC20\n"
+            f"Nom du contract: {name}\n"
+            f"Symbole: {symbol}\n"
+            f"Supply: {totalsupply}\n"
+            )
+        return True
+    except:
+        return False
+
+def is_erc721(contract_address):
+    erc721_contract = web3.eth.contract(address=contract_address, abi=abi_erc721)
+    try:
+        erc721_contract.functions.supportsInterface(
+            web3.keccak(text="supportsInterface(bytes4)").hex()[:10]  # Interface ERC721
+        ).call()
+        return True
+    except:
+        return False
+
+def main():
+    transactions = nb_block()
+    if transactions is not None:
         for tx in transactions:
             to_address = tx['to']
-            #It's smart contract
             if to_address is None or to_address == "0x0":
                 info_transac = web3.eth.get_transaction_receipt(tx.hash.hex())
-                address = info_transac['contractAddress']
-                contract = web3.eth.contract(address=address, abi=abi)
-                try:
-                    name = contract.functions.name().call()
-                    symbol = contract.functions.symbol().call()
-                    total_supply = contract.functions.totalSupply().call()
-                    is_erc20 = True
-                except:
-                    is_erc20 = False
-
-                if is_erc20:
+                contract_address = info_transac['contractAddress']
+                if is_erc20(contract_address):
                     print()
-                    print(address,"Ce contrat est ERC20")
-                    print('Nom du contrat: ', name)
-                    print('Symbole: ', symbol)
-                    print('Total Supply: ', total_supply)
-                    print()
-                else:
-                    print()
-                    print(address,"Ce contrat n'est pas un ERC20")
-                    print()
+                elif is_erc721(contract_address):
+                    print(f"{contract_address} - Ce contrat est un ERC721")
 
 while __name__ == '__main__':
     main()
-
 
 
  
